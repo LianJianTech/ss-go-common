cf8b0dadd98328e1b90e2fe6947e22c12db04a62
vincecfl <vincecfl@gmail.com>
2022-07-02 20:05:28 +0800

    update code

--- errno/code.go
+++ errno/code.go
@@ -1,8 +1,10 @@
 package errno
 
 var (
-	OK          = &Errno{Code: 0, Msg: "ok"}
-	AuthError   = &Errno{Code: 1, Msg: "auth error"}
-	ParamError  = &Errno{Code: 2, Msg: "request param error"}
-	HandleError = &Errno{Code: 3, Msg: "handle error"}
+	OK           = &Errno{Code: 0, Msg: "ok"}
+	ParamError   = &Errno{Code: 1, Msg: "request param error"}
+	TokenError   = &Errno{Code: 2, Msg: "token error"}
+	AccountError = &Errno{Code: 3, Msg: "account or password error"}
+	HandleError  = &Errno{Code: 4, Msg: "handle error"}
+	OtherError   = &Errno{Code: 5, Msg: "other error"}
 )
